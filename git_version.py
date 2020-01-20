219b51579a7a152d48cb54521e2385962869ccd5
Zahari <zaharid@gmail.com>
2020-01-20 21:47:05 +0000

    Simplify git version

--- git_version.py
+++ git_version.py
@@ -6,10 +6,11 @@ def simple_call(s: str) -> str:
         s.split(), capture_output=True, text=True, check=True
     ).stdout.strip()
 
-def version_from_git():
-    last_tag_hash = simple_call('git rev-list --tags --max-count=1')
-    last_tag = simple_call(f'git describe --tags {last_tag_hash}')
-    last_rev = simple_call(f'git rev-list --count {last_tag_hash}..')
-    return f'{last_tag}.{last_rev}'
 
-print(version_from_git())
+def version_from_git():
+    tag, rev, hashstr = simple_call('git describe --tags --dirty').split('-', 2)
+    return f'{tag}.{rev}+{hashstr}'
+
+
+__version__ = version_from_git()
+print(__version__)
